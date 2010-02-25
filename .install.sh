da71dd1161f284e0f79e227efea6c0c6402fd0d5
wizard <kwargs@gmail.com>
2010-02-25 18:18:06 +0300

    don't override conf files

--- .install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for f in `ls`; do
-    echo $f;
-    ln -s `pwd`/$f $HOME/.$f
-done;
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for f in `ls|grep -v install| grep -v README`; do
+    to_file=$HOME/.$f;
+    if [ -f "$to_file" ]; then
+        echo "file $to_file exsits, save to $to_file.prev";
+        mv $to_file $to_file.prev;
+    fi;
+    ln -sf `pwd`/$f $to_file;
+done;
