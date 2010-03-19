02073996713c1e1085d41a15719c17bca3fb78d3
Alexander K <kwargs@gmail.com>
2010-03-19 15:03:34 +0300

    force remove

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ for f in `ls|grep -v install| grep -v README`; do
     to_file=$HOME/.$f;
     if [ -f "$to_file" ]; then
         #echo "file $to_file exsits, save to $to_file.prev";
-        rm $to_file;
+        rm -rf $to_file;
         # mv $to_file $to_file.prev;
     fi;
     ln -sf `pwd`/$f $to_file;
