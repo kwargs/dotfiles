a9bf72b4df9e1e7c2f1f712ce2914dda5b1e5697
Alexander K <kwargs@gmail.com>
2010-04-06 11:10:53 +0400

    + README

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 
 for f in `ls|grep -v install| grep -v README`; do
     to_file=$HOME/.$f;
-    if [ -f "$to_file" ]; then
-        #echo "file $to_file exsits, save to $to_file.prev";
+    if [ -e "$to_file" ]; then
         rm -rf $to_file;
-        # mv $to_file $to_file.prev;
     fi;
     ln -sf `pwd`/$f $to_file;
 done;
