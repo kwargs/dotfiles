c790056b7df8f6f9c1011da2387f032bd3a81eaa
Alexander K <kwargs@gmail.com>
2010-04-19 16:44:10 +0400

    + SQL*plus config

---
+++ sql/login.sql
@@ -0,0 +1,13 @@
+define _editor=vim
+-- http://www.citforum.ru/database/oracle/sqlplus/
+set termout off
+set appinfo on
+set termout on
+
+set pagesize 64
+set linesize 128
+set numformat 99999999999999999999
+
+set sqlprompt "_USER'@'_CONNECT_IDENTIFIER> "
+
+-- vim: set syntax=plsql :
